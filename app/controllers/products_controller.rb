11b3386b8eb7dcfb63ce9f4ed82fc009888fc338
Santiago Mesa <104391164+altair3542@users.noreply.github.com>
2023-09-24 22:30:17 -0500

    refactor 1
    time to refactor. making use fo DRY, fix some code.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,11 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.all
+    @products = Product.all.with_attached_photo
   end
 
   def show
-    @product = Product.find(params[:id])
+    product
   end
 
   def new
@@ -22,12 +22,11 @@ class ProductsController < ApplicationController
   end
 
   def edit
-    @product = Product.find(params[:id])
+    product
   end
 
   def update
-    @product = Product.find(params[:id])
-    if @product.update(product_params)
+    if product.update(product_params)
       redirect_to products_path, notice: 'Tu producto se ha actualizado correctamente'
     else
       render :edit, status: :unprocessable_entity
@@ -35,8 +34,7 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    @product = Product.find(params[:id])
-    @product.destroy
+    product.destroy
     redirect_to products_path, notice: 'Tu producto se ha eliminado correctamente', status: :see_other
   end
 
@@ -44,5 +42,9 @@ class ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:title, :description, :price, :photo)
+  end
+
+  def product
+    @product = Product.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  delete '/products/:id', to: 'products#destroy', as: :destroy_product
-  patch '/products/:id', to: 'products#update'
-  post '/products', to: 'products#create'
-  get '/products/new', to: 'products#new', as: :new_product
-  get '/products', to: 'products#index'
-  get '/products/:id', to: 'products#show', as: :product
-  get '/products/:id/edit', to: 'products#edit', as: :edit_product
+  resources :products, path: '/'
 
   # Defines the root path route ("/")
   # root "articles#index"
