54c338e73555de1cd5fec4a17280dbfec374b04e
Santiago Mesa <104391164+altair3542@users.noreply.github.com>
2023-09-24 20:00:42 -0500

    creating show logic
    in this commit we upload the logic for the personalized page for our products.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/products", to: "products#index"
+  get "/products/:id", to: "products#show", as: :product
   # Defines the root path route ("/")
   # root "articles#index"
 end
