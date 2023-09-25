c823d012503798c10cecbb232cb263b87f8fa2df
Santiago Mesa <104391164+altair3542@users.noreply.github.com>
2023-09-25 09:59:21 -0500

    adding full text search feature.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ApplicationRecord
+  include PgSearch::Model
+  pg_search_scope :search_full_text, against: {
+    title: 'A',
+    description: 'B'
+  }
+
+  
   has_one_attached :photo
   validates :title, :description, :price, presence: true
   belongs_to :category
