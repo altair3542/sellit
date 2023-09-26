341bfb00429c0626ca918bb8072396c3404b1697
Santiago Mesa <104391164+altair3542@users.noreply.github.com>
2023-09-26 11:02:56 -0500

    query object pattern

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,24 +1,7 @@
 class ProductsController < ApplicationController
-
   def index
     @categories = Category.order(name: :asc).load_async
-    @products = Product.with_attached_photo
-    if params[:category_id]
-      @products = @products.where(category_id: params[:category_id])
-    end
-    if params[:min_price].present?
-      @products = @products.where('price >= ?', params[:min_price])
-    end
-    if params[:max_price].present?
-      @products = @products.where('price <= ?', params[:max_price])
-    end
-    if params[:query_text].present?
-      @products = @products.search_full_text(params[:query_text])
-    end
-    order_by = Product::ORDER_BY.fetch(params[:order_by]&.to_sym, Product::ORDER_BY[:newest])
-    @products = @products.order(order_by).load_async
-
-    @pagy, @products = pagy_countless(@products, items: 12)
+    @pagy, @products = pagy_countless(FindProducts.new.call(params).load_async, items: 12)
   end
 
   def show
@@ -31,6 +14,7 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
+
     if @product.save
       redirect_to products_path, notice: t('.created')
     else
@@ -52,6 +36,7 @@ class ProductsController < ApplicationController
 
   def destroy
     product.destroy
+
     redirect_to products_path, notice: t('.destroyed'), status: :see_other
   end
 
