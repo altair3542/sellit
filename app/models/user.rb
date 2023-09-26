d6d4659a0fd9b419f95008959a457e5b2cca47a7
Santiago Mesa <104391164+altair3542@users.noreply.github.com>
2023-09-26 13:04:30 -0500

    making a user register tool without devise

---
+++ app/models/user.rb
@@ -0,0 +1,29 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: true,
+    format: {
+      with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
+      message: :invalid
+    }
+
+  validates :username, presence: true, uniqueness: true,
+    length: { in: 3..15 },
+    format: {
+      with: /\A[a-z-A-Z-0-9]+\z/,
+      message: :invalid
+    }
+
+  validates :password, length: { minimum: 6 }
+
+
+  before_save :downcase_atributes
+
+  private
+
+  def downcase_atributes
+    self.username = username.downcase
+    self.email = email.downcase
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   resources :categories, except: :show
   resources :products, path: '/'
 
+  namespace :authentication, path: '', as: '' do
+    resources :users, only: [:new, :create]
+  end
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
