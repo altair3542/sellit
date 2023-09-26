613ed520c970ea0a6adfa9a7971a1610c1b3462e
Santiago Mesa <104391164+altair3542@users.noreply.github.com>
2023-09-26 18:51:46 -0500

    adding a profile area

--- app/queries/find_products.rb
+++ app/queries/find_products.rb
@@ -11,6 +11,7 @@ class FindProducts
     scoped = filter_by_min_price(scoped, params[:min_price])
     scoped = filter_by_max_price(scoped, params[:max_price])
     scoped = filter_by_query_text(scoped, params[:query_text])
+    scoped = filter_by_user_id(scoped, params[:user_id])
     sort(scoped, params[:order_by])
   end
 
@@ -44,8 +45,16 @@ class FindProducts
     scoped.search_full_text(query_text)
   end
 
+  def filter_by_user_id(scoped, user_id)
+    return scoped unless user_id.present?
+
+    scoped.where(user_id: user_id)
+  end
+
   def sort(scoped, order_by)
     order_by_query = Product::ORDER_BY.fetch(order_by&.to_sym, Product::ORDER_BY[:newest])
     scoped.order(order_by_query)
   end
-end 
+
+
+end
