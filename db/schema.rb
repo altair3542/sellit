8cfc42ffccfc5cedf284820c5311b7fb636afa80
Santiago Mesa <104391164+altair3542@users.noreply.github.com>
2023-09-26 12:07:08 -0500

    minor bug fixing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_25_043641) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_26_162104) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,7 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_25_043641) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
